a623a39bbdc07db52a1dd381d48a64527079c3ea
tds4surbhit <surbhit.4zeracing@gmail.com>
2022-08-17 02:48:13 +0530

    Recursion

--- src/recursion1/Power.java
+++ src/recursion1/Power.java
@@ -1,10 +1,27 @@
 package recursion1;
 
+import java.util.Scanner;
+
 public class Power {
+	
+	public static int power(int x, int n) {
+		if(n == 0) {
+			return 1;
+		}
+		
+		int shortAnswer = power(x,n-1);
+		int finalAnswer = shortAnswer * x;
+		return finalAnswer ;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		sc.close();
+		int finalPower = power(n,x);
+		System.out.println(finalPower);
 	}
 
 }
